d5434220e55cde7ce0bd1f005cbfb75f44882780
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-08-07 15:49:33 +0530

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,18 +1,21 @@
 export default async function handler(req, res) {
+  const lavalinkHost = "http://pnode1.danbot.host:1186/v4/stats";
+  const password = "Yuvraj.apk#001";
+
   try {
-    const response = await fetch("http://pnode1.danbot.host:1186/v4/stats", {
+    const response = await fetch(lavalinkHost, {
       headers: {
-        Authorization: "Yuvraj.apk#001",
-      },
+        Authorization: password
+      }
     });
 
     if (!response.ok) {
-      return res.status(response.status).json({ error: "Failed to fetch Lavalink stats." });
+      return res.status(500).json({ error: "Failed to fetch Lavalink stats" });
     }
 
     const data = await response.json();
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Server error fetching stats." });
+    return res.status(500).json({ error: "Error fetching Lavalink stats" });
   }
 }
